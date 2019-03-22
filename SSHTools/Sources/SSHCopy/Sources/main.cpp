3978391fb9ebf8be2b0bf1c9486c3752db71d0d6
Zingam <zingam@outlook.com>
2019-03-23 00:42:20 +0200

    Qt and libssh compilation & deployment test

--- SSHTools/Sources/SSHCopy/Sources/main.cpp
+++ SSHTools/Sources/SSHCopy/Sources/main.cpp
@@ -4,6 +4,9 @@
 
 #include <libssh/libssh.h>
 
+#include <QApplication>
+#include <QWidget>
+
 #include <iostream>
 
 int
@@ -13,4 +16,17 @@ main(int argc, char* argv[])
   std::cout << "Profile folder location:\n";
   std::cout << "  " << Common::GetSystemPathname(Common::SystemPath::Profile)
             << "\n";
+
+  ssh_session my_ssh_session = ssh_new();
+  if (my_ssh_session == NULL)
+    exit(-1);
+  
+  QApplication app(argc, argv);
+
+  QWidget window;
+  window.resize(320, 240);
+  window.show();
+  window.setWindowTitle(QWidget::tr("SSHCopy"));
+
+  return app.exec();
 }
